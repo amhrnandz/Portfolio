60bdd05dc0b7d4372dc03893dcb6f8bf827ea926
Angel Hernandez <hrnandz.angel@gmail.com>
2025-07-17 16:13:42 +0800

    Setup portfolio

---
+++ script.js
@@ -0,0 +1,26 @@
+// For light and dark theme
+const content = document.querySelector("#content");
+const theme_switch = document.querySelector("#theme-switch");
+
+theme_switch.addEventListener("click", () => {
+    content.classList.toggle('dark-theme');
+})
+
+// For tab selection
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(selected => selected.addEventListener("click", (e) => {
+    buttons.forEach(button => {
+        const button_id = button.getAttribute('id');
+        const content = document.getElementById(`content-${button_id}`);
+        if (button === selected) {
+            button.classList.add("button--selected");
+            content.classList.remove("content--hidden");
+        } else {
+            button.classList.remove("button--selected");
+            content.classList.add("content--hidden");
+        }
+    })
+})
+
+)
